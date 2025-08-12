9e91ca2fde41ca3cf6f0fe324e6bd1f71b73a5eb
keilander <Kurt.Eilander@osii.com>
2025-08-12 11:33:42 -0500

    Made htmlTypes smarter

--- htmlTypes.py
+++ htmlTypes.py
@@ -1,10 +1,20 @@
 """
 Stubs and types for html
+
+Can support lxml and/or htmltools if installed
 """
 import typing
 import xml.dom.minidom
-import lxml.etree
+MinidomElement=xml.dom.minidom.Element
+try:
+    import lxml.etree
+    LxmlElement=lxml.etree.Element
+except ImportError:
+    LxmlElement=MinidomElement
+try:
+    from htmlTools import HtmlCompatible
+except ImportError:
+    HtmlCompatible=MinidomElement
 
-
-HtmlElementLike=typing.Union[lxml.etree.Element,xml.dom.minidom.Element]
-HtmlElementsLike=typing.Union[HtmlElementLike,typing.Iterable[HtmlElementLike]]
+HtmlElementLike=typing.Union[LxmlElement,MinidomElement]
+HtmlElementsLike=typing.Union[HtmlElementLike,typing.Iterable[HtmlElementLike],HtmlCompatible]
